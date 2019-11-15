497109997947d623b34bd81e6e634550d5cc9082
hiya <50945917+heesu40@users.noreply.github.com>
2019-11-15 17:41:02 +0900

    장고 crud

--- Python/Framword(Django)/django_orm/boards/urls.py
+++ Python/Framword(Django)/django_orm/boards/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('subwayresult/' , views.subwayresult),
     path('subway/' , views.subway),
     path('' , views.index),
+    path('subwayid/<int:id>/' , views.subwayid),
 ]
